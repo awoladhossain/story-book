1d0f9dde31ee067b8f065f40ff48bf84c696a0bd
Awolad Hossain <awoladh04@gmail.com>
2025-05-28 22:49:35 +0600

    bug fixes

--- server/index.js
+++ server/index.js
@@ -6,14 +6,27 @@ import connectDB from "./db/db.js";
 import authRouter from "./routers/auth.router.js";
 import travelRouter from "./routers/travel.router.js";
 import cors from "cors"
+
+
 dotenv.config();
 connectDB();
+
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your frontend URL
+    credentials: true,
+  })
+);
+
+
+
 // app.use("/uploads",express.static(path.join(__dirname,"./uploads")))
 app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
 app.use("/assets", express.static(path.join(path.resolve(), "assets")));
--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.get("/check-auth", verifyToken, checkAuth); // *check auth is for get user* /
 router.post("/login", login);
 router.post("/signup", signup);
-router.post("/logout", logout);
+router.post("/logout" ,logout);
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
